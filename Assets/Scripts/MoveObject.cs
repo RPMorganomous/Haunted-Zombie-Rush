3269baa43a0f28c0ea3f6732cb69b3996970d704
Richard Morgan <mba.rick@gmail.com>
2016-12-29 09:51:36 -0600

    Initial Commit
    Files up to the point of adding new features.

---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+
+    [SerializeField] private float objectSpeed = 1;
+    [SerializeField] private float resetPosition = -20.6f;
+    [SerializeField] private float startPosition = 76.23f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+
+        if (!GameManager.instance.GameOver)
+        {
+            transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime), Space.World);
+            //transform.Translate(new Vector3(-1, 0, 0) * (objectSpeed * Time.deltaTime), Space.World);
+            //transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime)); ==== old line
+
+
+            if (transform.localPosition.x <= resetPosition)
+            {
+                Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+                transform.position = newPos;
+            }
+        }
+	}
+}
